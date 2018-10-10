29aaa1530551d279d27319734aa92625b1c28667
benk1 <kakengi_bernard@hotmail.com>
2018-10-10 21:57:34 +0300

    add final function

--- form.js
+++ form.js
@@ -1,26 +1,12 @@
-/*function validateName(){
-    let fullName = document.querySelector("#fullname").value;
-    if(fullName.length === 0){
-    producePrompt("Full Name is required","red")
-    return false;
-    }
-}
 
-function producePrompt(message,promptLocation,  color){
-    document.getElementById(promptLocation).innerHTML = message;
-    document.getElementById(promptLocation).style.color = color;
-}*/
-/*let fullName = document.querySelector("#fullname").value;
-let result = fullName.match(/^[a-zA-Z]*\s{1}[a-zA-Z]*$/g);*/
-
-const inputs = document.querySelectorAll ('input');
+const inputs = document.querySelectorAll('input[type="text"],input[type="email"], textarea');
 const button = document.querySelector ('button');
 
-console.log (inputs);
+//console.log (inputs);
 
 const patterns = {
   fullname: /^[a-zA-Z]{7}\s{1}[a-zA-Z]{7}$/,
-  email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i,
+  //email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i,
   postcode:  /^[0-9]{5}$/,  
   country: /^[a-zA-Z]{5,15}$/,
   city: /^[a-zA-Z]{5,15}$/,
@@ -29,11 +15,21 @@ const patterns = {
   email: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
   reemail: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
   address: /^[a-zA-Z]{1,15}[\s][0-9]{1}[a-zA-Z]{1}[\s][0-9]$/g,
-  infobox:  /[a-zA-Z0-9]+/,
+  infobox:  /^[a-zA-Z]{10}/,
   HowDidYouHearAboutUs: /^[A-Za-z]{30}/,
   potfolio: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/
   
 };
+
+
+
+let arryInputs = Array.from(inputs);
+
+function validInputs() {
+  return arryInputs.every(input=>{
+          return input.className == 'valid'   
+  });  
+} 
 
 inputs.forEach (element => {
   element.addEventListener ('input', e => {
@@ -44,8 +40,10 @@ inputs.forEach (element => {
 const formValidator = (element, pattern) => {
   if (pattern.test (element.value)) {
     element.className = 'valid';
-    button.className = 'submit-button';
-    button.disabled = false;
+    if(validInputs()){
+      button.className = 'submit-button';
+      button.disabled = false;
+    }   
   } else {
     element.className = 'invalid';
     button.classList.remove ('submit-button');
