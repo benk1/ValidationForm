c250552229c501c62c369fa99e4440bae5278aa4
benk1 <kakengi_bernard@hotmail.com>
2018-10-12 11:58:56 +0300

    final check

--- form.js
+++ form.js
@@ -1,31 +1,31 @@
 
-const inputs = document.querySelectorAll('input[type="text"],input[type="email"], textarea');
-const button = document.querySelector ('button');
+const inputs = document.querySelectorAll('input[type="text"],input[type="email"],input[type="url"], textarea');
+const button = document.querySelector ('#button1');
 
 //console.log (inputs);
 
 const patterns = {
-  fullname: /^[a-zA-Z]{7}\s{1}[a-zA-Z]{7}$/,
+  fullname: /^[a-zA-Z]{1,7}\s{1}[a-zA-Z]{1,7}$/,
   //email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i,
   postcode:  /^[0-9]{5}$/,  
   country: /^[a-zA-Z]{5,15}$/,
   city: /^[a-zA-Z]{5,15}$/,
   state: /^[a-zA-Z]{5,15}$/,
-  phone: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\0-9]*$/g,
+  phone: /^[+]{1}[0-9]{1,12}$/,
   email: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
   reemail: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
-  address: /^[a-zA-Z]{1,15}[\s][0-9]{1}[a-zA-Z]{1}[\s][0-9]$/g,
-  infobox:  /^[a-zA-Z ]{50}$/,
-  HowDidYouHearAboutUs: /^[A-Za-z ]{10,50}$/,
-  potfolio: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g
+  address: /^[a-zA-Z ]{1,15}[0-9]{1,9}[a-zA-Z ]{1}[0-9 ]{1,9}$/,
+  infobox:  /^[a-zA-Z ]{1,50}$/,
+  HowDidYouHearAboutUs: /^[A-Za-z ]{1,50}$/,
+  potfolio: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{1,4}(:[0-9]{1,5})?(\.*)?$/g,
   
 };
 
 
 
 let arrayInputs = Array.from(inputs);
-
-function validInputs() {
+const validInputs = () => {
+//function validInputs() {
   return arrayInputs.every(input=>{
           return input.className == 'valid'   
   });  
